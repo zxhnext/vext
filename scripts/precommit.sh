448862256ef511520cc4f2fa74491df693630aa6
赵逍涵 <xiaohannext@qq.com>
2020-06-28 14:24:02 +0800

    fix:去掉骨架屏中的mini-css配置

--- scripts/precommit.sh
+++ scripts/precommit.sh
@@ -1,2 +1,10 @@
+###
+ # @Author: your name
+ # @Date: 2020-06-20 21:54:49
+ # @LastEditTime: 2020-06-28 14:23:16
+ # @LastEditors: your name
+ # @Description: In User Settings Edit
+ # @FilePath: /vext/scripts/precommit.sh
+### 
 # 只对本次提交做eslint检测
-lint-staged
+# lint-staged
